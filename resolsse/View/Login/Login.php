228222a546a1860f81d7dfe34f9bf44c51a9fa9e
icaro2222 <icarodias2222@gmail.com>
2022-03-26 20:43:26 -0300

    .

---
+++ resolsse/View/Login/Login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<link rel="shortcut icon" href="../../../public/img/icons/favicon1.png" type="image/x-icon">
+	<style type="text/css">
+		body{background-color: rgb(172, 216, 209);}
+
+		#corpo{width: 1000px; background-color: aliceblue; margin: 0%;}
+	</style>
+    <meta charset="UTF-8">
+	<title>Login</title>
+</head>
+<body>
+	<div>
+		<div class="list-group">
+			<div>
+				<h3>Login</h3>
+				<img src="https://www.educabras.com/media/faculdades/image/ifbaiano.png">
+				<img src="https://www.ifbaiano.edu.br/unidades/bonfim/files/2011/05/Academia.png">
+			</div>
+		</div>
+	</div>
+</body>
+</html>
