c1869148b1272a05243c41d37d8803b59412d3dd
icaro2222 <icarodias2222@gmail.com>
2022-03-26 13:54:03 -0300

    estruturando

---
+++ resolsse/View/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SysGymIf</title>
+</head>
+<body>
+    <div>
+        <form action="">
+            <input type="text">
+            <input type="text">
+            <input type="text">
+            <input type="text">
+        </form>
+    </div>
+</body>
+</html>
+
+<?php
+    echo 'oi ';
+?>
