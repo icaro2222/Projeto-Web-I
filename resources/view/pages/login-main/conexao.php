2ee5ee91923460f913d72a11d34cab1b85414592
icaro2222 <icarodias2222@gmail.com>
2022-05-10 16:24:31 -0300

    :octocat:

--- resources/view/pages/login-main/conexao.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('HOST','127.0.0.1');
-define('USUARIO','root');
-define('SENHA','');
-define('DB','login');
-$conexao = mysqli_connect(HOST,USUARIO,SENHA,DB) or die('nao foi possivel connectar');
