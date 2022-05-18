9608448af0d4bf0d5568b577eef8c86be1eb256d
icaro2222 <icarodias2222@gmail.com>
2022-05-18 14:19:26 -0300

    organizando pastas

--- Documentos/IFheath.sql
+++ Documentos/IFheath.sql
@@ -6,17 +6,9 @@ USE IFhealth;
 
 CREATE TABLE usuario(
 	idUsuario int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(45),
+	usuario VARCHAR(45),
 	senha VARCHAR(45),
-	num_registro INT(14)
-);
-
-
-CREATE TABLE Discente(
-	idDiscente int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(45),
-	senha VARCHAR(45),
-	numMatricula VARCHAR(14),
+	num_registro INT(14),
 	peso FLOAT,
 	sexo VARCHAR(20),
 	idade INT,
@@ -24,39 +16,39 @@ CREATE TABLE Discente(
 	bloqueado BOOLEAN
 );
 
-CREATE TABLE Disponibilidade(
+CREATE TABLE disponibilidade(
 	idDisponibilidade int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	dia date,
 	hora time,
 	livre BOOLEAN
 );
 
-CREATE TABLE Agendamento(
+CREATE TABLE agendamento(
 	idAgendamento int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	fkTutor INT,
 	fkDiscente INT,
 	fkDisponibilidade INT
 );
 
-CREATE TABLE Anotacoes(
+CREATE TABLE anotacoes(
 	idAnotacoes int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	descricao TEXT
 );
 
-CREATE TABLE Bloqueio(
+CREATE TABLE bloqueio(
 	idBloqueio int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	idDiscente INT,
 	idTutor INT
 	
 );
 
-ALTER TABLE Agendamento ADD CONSTRAINT `fk_Agendamento_idTutor` 
-FOREIGN KEY (fkTutor) REFERENCES Usuario(idUsuario);
+ALTER TABLE agendamento ADD CONSTRAINT `fk_Agendamento_idTutor` 
+FOREIGN KEY (fkTutor) REFERENCES usuario(idUsuario);
 
-ALTER TABLE Agendamento ADD CONSTRAINT `fk_Agendamento_idDisponivel` 
-FOREIGN KEY (fkDisponibilidade) REFERENCES Disponibilidade(idDisponibilidade);
+ALTER TABLE agendamento ADD CONSTRAINT `fk_Agendamento_idDisponivel` 
+FOREIGN KEY (fkDisponibilidade) REFERENCES disponibilidade(idDisponibilidade);
 
 
-ALTER TABLE Agendamento ADD CONSTRAINT `fk_Agendamento_idDiscente` 
-FOREIGN KEY (fkDiscente) REFERENCES Discente(idDiscente);
+ALTER TABLE agendamento ADD CONSTRAINT `fk_Agendamento_idDiscente` 
+FOREIGN KEY (fkDiscente) REFERENCES usuario(idusuario);
 
