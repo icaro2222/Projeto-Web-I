77d125bb2cbeda905590d4424d2fd5e6cf558469
icaro2222 <icarodias2222@gmail.com>
2022-05-21 17:21:19 -0300

    .

--- login.php
+++ login.php
@@ -4,14 +4,14 @@ ini_set("display_errors", 1);
 
 session_start();
 
-require_once('./app/controller/Discente.php');
+require_once('./app/controller/Usuario.php');
 
 if(empty($_POST['usuario']) || empty($_POST['senha'])){
     header('location:index.php');
     exit();
 }
 
-$usuario = new Discente;
+$usuario = new Usuario;
 $usuario->setNome($_POST['usuario']);
 $usuario->setSenha($_POST['senha']);
 
