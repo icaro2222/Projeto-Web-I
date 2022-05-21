a54dbd6888b26c8fae771e952d796125621cfb16
icaro2222 <icarodias2222@gmail.com>
2022-05-21 18:45:42 -0300

    zero

--- app/constant/constant.php
+++
@@ -1,4 +0,0 @@
-<?php
-//usei o __FILE__ ao invés de __DIR__ devido a algunas questões de retrocompatibilidade
-
-define('ROOT_PATH', dirname(__FILE__));
--- model/db/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('HOST','127.0.0.1');
-define('USER','icaro');
-define('PASS','tatakae');
-define('BASE','IFhealth');
-?>
