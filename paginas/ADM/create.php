bd6bcac7174602b8af79639c6e33d6a3a35e0fa7
cloves-max <clovesjuniorrodrigues@gmail.com>
2022-05-21 19:49:08 -0300

    first commit

---
+++ paginas/ADM/create.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>AdmTela4</title>
+	<link rel="stylesheet" type="text/css" href="css/styleTela4.css">
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+	<section>
+		<div class="container">
+			<div class="add-tutor">
+				<h1>Adicionar tutor:</h1>
+				<div class="add-tutor-cadast">
+					<div class="cap1"><p>Nome:</p>
+					<textarea></textarea></div>
+					<div class="cap2"><p>Matricula:</p>
+					<textarea></textarea></div>
+					<div class="cap3"><p>Digite o usuário:</p>
+					<textarea></textarea></div>
+					<div class="cap4"><p>Digite a senha:</p>
+					<textarea></textarea></div>
+					
+				</div><!--add-tutor-cadast-->
+				<h2>Selecione os horarios do instrutor:</h2>
+				<div class="add-tutor-horari">
+					<div class="select">
+					<div class="select-data">
+						<select name="Data"><option>1</option>
+						<option>2</option></select>
+						<p>Data:</p></div><!--select-data-->
+					<div class="select-hora">
+						<select name="Hora"><option>1</option>
+						<option>2</option></select><p>Hora:</p></div><!--select-hora-->
+					<div class="select-min">
+						<select name="Hora"><option>1</option>
+						<option>2</option></select><p>Minuto:</p></div><!--select-min-->
+				</div><!--select-->
+				<div class="botão-agendamento">
+					<input type="submit" name="Cadastrar" value="Cadastrar">
+					</div><!--botão-agendamento-->
+				</div><!--add-tutor-horari-->
+			</div><!--add-tutor-->
+		</div><!--container-->
+	</section>
+
+
+
+
+
+</body>
+</html>
