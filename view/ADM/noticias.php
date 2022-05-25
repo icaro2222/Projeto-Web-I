69a18813d3f71a0c46dc8388d0833046a6507935
icaro2222 <icarodias2222@gmail.com>
2022-05-25 19:50:48 -0300

    criando crud de noticias

--- view/ADM/noticias.php
+++ view/ADM/noticias.php
@@ -1,3 +1,11 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+require_once('../../app/controller/Aviso.php');
+
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -10,31 +18,74 @@
 </head>
 
 <body>
+    
+<?php
+	$aviso = new Aviso;
+	if(isset($_POST['Adicionar'])){
+
+        $descricao = $_POST['descricao'];
+
+		$aviso->setDescricao($descricao);
+		
+		if($aviso->insert()){
+			echo "Aviso ". $descricao. " inserido com sucesso";
+		}
+    }
+    ?>
+
     <section>
         <div class="container">
             <div class="notas">
                 <h1>Notas</h1>
-                <p>* A bobrinha 1: Proin vulputate tellus urna, ac tempus orci auctor vel. Proin viverra sagittis porta. Quisque eget rhoncus est. Nunc ac nisi eu eros sollicitudin semper. Aenean feugiat ante non nisl semper, non aliquet elit ornare. Morbi pharetra nec justo sed vehicula.</p>
-                <p>* A bobrinha 2: Proin vulputate tellus urna, ac tempus orci auctor vel. Proin viverra sagittis porta. Quisque eget rhoncus est. Nunc ac nisi eu eros sollicitudin semper. Aenean feugiat ante non nisl semper, non aliquet elit ornare. Morbi pharetra nec justo sed vehicula.</p>
-                <p>* A bobrinha 3: Proin vulputate tellus urna, ac tempus orci auctor vel. Proin viverra sagittis porta. Quisque eget rhoncus est. Nunc ac nisi eu eros sollicitudin semper. Aenean feugiat ante non nisl semper, non aliquet elit ornare. Morbi pharetra nec justo sed vehicula.</p>
-                <p>* A bobrinha 4: Proin vulputate tellus urna, ac tempus orci auctor vel. Proin viverra sagittis porta. Quisque eget rhoncus est. Nunc ac nisi eu eros sollicitudin semper. Aenean feugiat ante non nisl semper, non aliquet elit ornare. Morbi pharetra nec justo sed vehicula.</p>
+                <?php
+                $avisos = $aviso->findAll();
+                foreach ($avisos as $key => $value) {?>
+                    <p><?php echo $value->descricao;?></p>
+                <?php
+                }
+                ?>
             </div>
             <!--notas-->
         </div>
         <!--container-->
     </section>
 
-    <section>
-        <div class="container">
+	<section>
+		<div class="container">
+			<div class="editar-nota">
+				<h1>Editar as notas:</h1>
+				<div class="adicionar-nota">
+					<h2>Adicionar:</h2>
+                    <form action="" method="POST">
+					<div class="texto-add-nota">
+						<textarea name="descricao"></textarea>
+						<div class="tad-btt">
+							<input type="submit" name="Adicionar" value="Adicionar">
+						</div>
+					</div><!--texto-add-nota-->
+					</form>
+				</div><!--adicionar-nota-->
 
-            <div class="tad-btt">
-                <input type="submit" name="Adicionar" value="Adicionar">
-            </div>
+				<div class="apagar-edd">
+					<div class="apagar1">
+						<h2>Apagar ou editar:</h1>
+						<p>Selecione a nota:</p>
+						<select><option>1</option><option>2</option></select>
+					</div><!--apagar1-->
+					<div class="apagar2">
+						<textarea></textarea>
+						<div class="apagar3">
+						<input type="submit" name="Remover" value="Remover">
+						<input type="submit" name="salvar" value="Salvar">
+					</div>
+					</div><!--apagar2-->
+					
+				</div><!--apagar-edd-->
+				
+			</div><!--editar-nota-->
+		</div><!--container-->
+	</section>
 
-
-        </div>
-        <!--container-->
-    </section>
 
 </body>
 
